cac672e02235cadba2166a05998b4ce4fd1aa4a4
antoineC <78803429+antoine59164@users.noreply.github.com>
2022-01-13 19:43:03 +0100

    Add files via upload

---
+++ src/Probleme.java
@@ -0,0 +1,15 @@
+public class Probleme {
+    public final int idProbleme;
+
+    public Probleme(int idProbleme) {
+        this.idProbleme = idProbleme;
+    }
+
+    public void lancerProbleme(){
+
+    }
+
+    public void modifier(){
+
+    }
+}
