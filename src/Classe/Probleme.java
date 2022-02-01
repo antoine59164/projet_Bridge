1c63427581c5b6ecaac4a9b1ef42d6460f6a295b
Antoine Caudron <antoinecaudron8@gmail.com>
2022-02-01 19:19:57 +0100

    Ajout

---
+++ src/Classe/Probleme.java
@@ -0,0 +1,17 @@
+package Classe;
+
+public class Probleme {
+    public final int idProbleme;
+
+    public Probleme(int idProbleme) {
+        this.idProbleme = idProbleme;
+    }
+
+    public void lancerProbleme(){
+
+    }
+
+    public void modifier(){
+
+    }
+}
